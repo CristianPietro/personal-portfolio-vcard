c4259f0118df0a70edbf0766ea424bbb0e044015
CristianPietro <crpietro26@gmail.com>
2022-11-06 12:34:48 -0300

    JS: navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,3 +94,44 @@ const filterFunc = function (selectedValue) {
     }
   }
 }
+
+// contact form variables
+
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+for (let i = 0; i < formInputs.length; i++){
+  formInputs[i].addEventListener("input", function (){
+
+    //chec form validation
+    if(form.checkValidity()){
+      formBtn.removeAttribute("disabled");
+    }else{
+      formBtn.setAttribute("disabled", "")
+    }
+  });
+}
+
+// page navigation
+
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const  pages = document.querySelectorAll("[data-page]");
+
+//add event to all nav link
+
+for(let i = 0; i < navigationLinks.length; i++){
+  navigationLinks[i].addEventListener("click", function (){
+
+   for (let i = 0; i < pages.length; i++){
+    if(this.innerHTML.toLowerCase() === pages[i].dataset.page){
+      pages[i].classList.add("active");
+      navigationLinks[i].classList.add("active");
+      window.scroll(0, 0);
+    }else{
+      pages[i].classList.remove("active");
+      navigationLinks[i].classList.remove("active");
+    }
+   }
+  });
+}
