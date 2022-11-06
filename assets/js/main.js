6ab70c19bf6f5968b0c648be847f3161a12e7ff4
CristianPietro <crpietro26@gmail.com>
2022-11-06 00:08:04 -0300

    90% contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ const elementToggleFunc = function (elem) {elem.classList.toggle('active')}
 
 //sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
-const sidebarbtn = document.querySelector("[data-sidebar-btn]");
+const sidebarbtn = document.querySelector("[data-sidebar-button]");
 
 //sidebar toggle functionality mobile
 sidebarbtn.addEventListener("click", () =>{elementToggleFunc(sidebar)});
